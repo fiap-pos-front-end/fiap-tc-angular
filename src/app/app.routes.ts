ac65d30773913753fe48304a272728b9c35753fd
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-29 15:56:39 -0300

    feat(teste): nova rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'home2', component: Home2Component },
 ];
