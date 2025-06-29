03507d02bcfc5424b51682fecbed690bc6cd2faf
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-29 17:25:56 -0300

    feat: primeng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private router = inject(Router);
+  goToAngular2() {
+    this.router.navigate(['/angular/home2']);
+  }
+}
