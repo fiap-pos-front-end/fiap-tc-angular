631f2accbeb30522aac9ccdb49ad7f00fd2cb4a5
João Vitor Veronese Vieira <joao.vieira@nursa.com>
2025-07-06 12:27:49 -0300

    feat: add /transferencias route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,11 @@ import { Home2Component } from './home2/home2.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home2', component: Home2Component },
+  {
+    path: 'transferencias',
+    loadComponent: () =>
+      import('./pages/transactions/transactions-page.component').then(
+        (m) => m.TransactionsPageComponent
+      ),
+  },
 ];
