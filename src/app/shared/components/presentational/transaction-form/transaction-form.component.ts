6d718fc5782258cd0c56d56a4c5bfc7b9ad99aba
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-08 09:49:46 -0300

    refactor: improving transaction-form

--- src/app/shared/components/presentational/transaction-form/transaction-form.component.ts
+++ src/app/shared/components/presentational/transaction-form/transaction-form.component.ts
@@ -1,21 +1,22 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Transaction, TransactionType } from '@fiap-tc-angular/core/domain';
-import { Button } from 'primeng/button';
+import { DatePicker } from 'primeng/datepicker';
 import { InputNumber } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
 
 @Component({
   selector: 'app-transaction-form',
-  imports: [Button, InputNumber, FormsModule, Select],
+  imports: [DatePicker, InputNumber, InputTextModule, FormsModule, Select],
   templateUrl: './transaction-form.component.html',
 })
 export class TransactionFormComponent {
-  readonly transactionTypes: TransactionType[] = Object.values(TransactionType);
+  transaction: WritableSignal<Transaction> = signal<Transaction>(Transaction.reset());
+  selectedTransactionType: TransactionType | undefined;
 
-  submitted: boolean = false;
-  transaction: WritableSignal<Transaction> = signal<Transaction>(
-    Transaction.create('ID-FAKE', TransactionType.INCOME, 0, new Date(), 'CATEGORY-FAKE'),
+  readonly transactionTypes: Array<{ label: TransactionType; value: string }> = Object.entries(TransactionType).map(
+    ([value, label]) => ({ label, value }),
   );
 
   hideDialog() {}
