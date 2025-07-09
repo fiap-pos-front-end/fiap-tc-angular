ffa3ba4cddc3fda3c80190a4d9ec39fc806b41fc
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-09 17:40:23 -0300

    evento compartilhado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { Home2Component } from './pages/home2/home2.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'home2', component: Home2Component },
-
-  // Lazy loading e carregamento assíncrono
   {
-    path: 'transferencias',
+    path: '',
     loadComponent: () =>
       import('./pages/transactions/transactions-page.component').then((m) => m.TransactionsPageComponent),
   },
--- src/app/pages/transactions/transactions-page.component.ts
+++ src/app/pages/transactions/transactions-page.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TransactionsListComponent } from '@fiap-tc-angular/components';
+import { emitEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 
 @Component({
   selector: 'app-transactions-page',
   templateUrl: './transactions-page.component.html',
   imports: [TransactionsListComponent],
 })
-export class TransactionsPageComponent {}
+export class TransactionsPageComponent implements OnInit {
+  ngOnInit(): void {
+    emitEvent('balanceChanged', 123);
+  }
+}
