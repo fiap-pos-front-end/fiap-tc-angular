55eb42ab030d66e7a6b7bd2bddc0dc83819d3380
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-09 08:49:11 -0300

    feat: create provider and concrete implementation of id generation with uuid

--- src/app/infrastructure/providers/index.ts
+++ src/app/infrastructure/providers/index.ts
@@ -1,2 +1,3 @@
 export * from './in-memory-transaction.provider';
 export * from './local-storage-transaction.provider';
+export * from './uuid-generator.provider';
--- src/app/infrastructure/repositories/index.ts
+++ src/app/infrastructure/repositories/index.ts
@@ -1,2 +1,3 @@
 export * from './in-memory-transaction.repository';
 export * from './local-storage-transaction.repository';
+export * from './uuid-generator.repository';
