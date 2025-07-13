5d3bdc2c8a90052f27e83679ecf28c9fc7019695
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-13 11:41:55 -0300

    refactor: use translation from shell instead of local

--- src/app/pages/transactions/transactions-page.component.ts
+++ src/app/pages/transactions/transactions-page.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { emitEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 import { TransactionsListComponent } from '@fiap-tc-angular/components';
 import { ID_GENERATOR_TOKEN, TRANSACTION_REPOSITORY_TOKEN } from '@fiap-tc-angular/core/application';
 import { InMemoryTransactionRepository, UuidGeneratorRepository } from '@fiap-tc-angular/infrastructure';
-import pt from 'primelocale/pt.json';
-import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-transactions-page',
@@ -16,10 +14,7 @@ import { PrimeNG } from 'primeng/config';
   ],
 })
 export class TransactionsPageComponent implements OnInit {
-  private readonly config = inject(PrimeNG);
-
   ngOnInit(): void {
-    this.config.setTranslation(pt.pt);
     emitEvent('balanceChange', 123);
   }
 }
