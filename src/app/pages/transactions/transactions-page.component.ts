0355ff12b496b6f929a2fc6a7150ba542d5fa10f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-13 11:04:41 -0300

    feat: add filters translated

--- src/app/pages/transactions/transactions-page.component.ts
+++ src/app/pages/transactions/transactions-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { emitEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 import { TransactionsListComponent } from '@fiap-tc-angular/components';
 import { ID_GENERATOR_TOKEN, TRANSACTION_REPOSITORY_TOKEN } from '@fiap-tc-angular/core/application';
 import { InMemoryTransactionRepository, UuidGeneratorRepository } from '@fiap-tc-angular/infrastructure';
+import pt from 'primelocale/pt.json';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-transactions-page',
@@ -14,7 +16,10 @@ import { InMemoryTransactionRepository, UuidGeneratorRepository } from '@fiap-tc
   ],
 })
 export class TransactionsPageComponent implements OnInit {
+  private readonly config = inject(PrimeNG);
+
   ngOnInit(): void {
+    this.config.setTranslation(pt.pt);
     emitEvent('balanceChange', 123);
   }
 }
