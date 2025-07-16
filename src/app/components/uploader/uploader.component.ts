2f2a6f7924346394dcfd684dd9d253087c9a4834
João Cardoso <joaoiancruz@gmail.com>
2025-07-15 23:49:08 -0300

    feature -> novo componente de uploader

---
+++ src/app/components/uploader/uploader.component.ts
@@ -0,0 +1,111 @@
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Component, Input} from '@angular/core';
+
+interface ArchiveItem {
+  source: SafeHtml;
+  ctdocume: string;
+  name: string;
+  type: string;
+  tpdocume: number;
+}
+
+@Component({
+  selector: 'app-uploader',
+  imports: [CommonModule],
+  templateUrl: './uploader.component.html',
+  styleUrl: './uploader.component.scss'
+})
+export class UploaderComponent {
+  typeAccepted = ['application/pdf', 'image/jpeg', 'image/png'];
+  arrObjArchive: ArchiveItem[] = [];
+  maxItems : number = 5;
+
+  @Input() context : any;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
+
+  async onSelectFile(data: FileList | any) {
+    let dados = (data.target?.files) ? data.target.files : (data.length > 0) ? (data) : null;
+    let arrArchivesError = [];
+    let cont = 0;
+
+    if (dados) {
+      for (let file of dados) {
+        let nameArchive = file.name;
+        try {
+          const result = await this.readFileAsDataURL(file);
+          if(!this.typeAccepted.find(f => file.type.includes(f.substring(1)))){
+            arrArchivesError.push(file.name);
+          }else if (this.arrObjArchive.find(i => i.source === result)) {
+            console.log(`Arquivo ${nameArchive} já inserido`, 'ERRO:');
+          } else {
+            let bufferLimpo = (result) ? result.split("base64,") : '';
+            this.arrObjArchive.push({
+               source: (this.formataTipos(file.type).string == 'pdf' ) ? this.getSafeHtml(result) : result, 
+               ctdocume: bufferLimpo[1], 
+               name: file.name, 
+               type: file.type,
+               tpdocume: this.formataTipos(file.type).id_type});
+          }
+          cont++;
+        } catch (error) {
+          console.error(`Erro ao ler o arquivo ${nameArchive}:`, error);
+        }
+      }
+
+      if(arrArchivesError.length == 1){
+        console.log(`O arquivo ${arrArchivesError[0]} possui um tipo não permitido`, 'ERRO:');
+      }else if (arrArchivesError.length > 1){
+        console.log(`Os arquivos ${arrArchivesError.join(', ')} possuem tipos não permitidos`, 'ERRO:');
+      }
+
+    }
+  }
+
+  handleDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  handleFileDrop(event: DragEvent): void {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    this.onSelectFile(files);
+  }
+
+  readFileAsDataURL(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
+  toFileList(files: any): FileList {
+    const dt = new DataTransfer();
+    files.forEach((f: any) => dt.items.add(f));
+    return dt.files;
+  }
+
+  getSafeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(html);
+  }
+
+  removerImg(index: number){
+    this.arrObjArchive.splice(index,1);
+  }
+
+  formataTipos(type: string){
+    if(['png','jpg','jpeg'].includes(type.split('/')[1])){
+      return {string:'imagem', id_type:1}
+    }else if(['pdf'].includes(type.split('/')[1])){
+      return {string:'pdf', id_type:2}
+    }else{
+      return {string:'Não Identificado', id_type:0}
+    }
+  }
+
+}
