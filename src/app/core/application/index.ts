6c82930fc040f8f6e536d0fcc8a21adf4475903b
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-19 01:25:22 -0300

    clean: adapt interfaces and delete old import

--- src/app/core/application/index.ts
+++ src/app/core/application/index.ts
@@ -1,1 +1,10 @@
+import { TransactionType } from '../domain';
+
 export * from './tokens';
+
+export interface CreateTransactionDTO {
+  type: TransactionType;
+  amount: number;
+  category: number;
+  date: Date;
+}
--- src/app/pages/transactions/transactions-page.component.ts
+++ src/app/pages/transactions/transactions-page.component.ts
@@ -1,17 +1,14 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TransactionsListComponent } from '@fiap-tc-angular/components';
-import { ID_GENERATOR_TOKEN, TRANSACTION_REPOSITORY_TOKEN } from '@fiap-tc-angular/core/application';
-import { ApiTransactionRepository, CategoryService, UuidGeneratorRepository } from '@fiap-tc-angular/infrastructure';
+import { ID_GENERATOR_TOKEN } from '@fiap-tc-angular/core/application';
+import { CategoryService, UuidGeneratorRepository } from '@fiap-tc-angular/infrastructure';
 
 @Component({
   selector: 'app-transactions-page',
   templateUrl: './transactions-page.component.html',
   imports: [AsyncPipe, TransactionsListComponent],
-  providers: [
-    { provide: TRANSACTION_REPOSITORY_TOKEN, useClass: ApiTransactionRepository },
-    { provide: ID_GENERATOR_TOKEN, useClass: UuidGeneratorRepository },
-  ],
+  providers: [{ provide: ID_GENERATOR_TOKEN, useClass: UuidGeneratorRepository }],
 })
 export class TransactionsPageComponent {
   private readonly categoryService = inject(CategoryService);
