b682dd799e81dda5bc18ad7f947232a025d32cbb
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-23 18:08:44 -0300

    feat: add statement component

--- src/app/pages/statement/statement-page.component.ts
+++ src/app/pages/statement/statement-page.component.ts
@@ -1,50 +1,65 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { emitEvent, EVENTS } from '@fiap-pos-front-end/fiap-tc-shared';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { emitEvent, EVENTS, Transaction, TransactionType } from '@fiap-pos-front-end/fiap-tc-shared';
 import { TransactionService } from '@fiap-tc-angular/infrastructure';
+import { MessageService } from 'primeng/api';
+import { DataView } from 'primeng/dataview';
+import { Tag } from 'primeng/tag';
 
 @Component({
   selector: 'app-statement',
-  imports: [],
+  imports: [DataView, Tag, CurrencyPipe, DatePipe],
+  providers: [MessageService],
   templateUrl: './statement-page.component.html',
 })
 export class StatementComponent implements OnInit {
-  private transactionService = inject(TransactionService);
+  private readonly messageService = inject(MessageService);
+  private readonly transactionService = inject(TransactionService);
+
+  readonly TransactionType = TransactionType;
+  readonly transactions = signal<Transaction[]>([]);
 
   ngOnInit(): void {
-    this.getAllTransactions();
+    this.loadTransactions();
   }
 
-  private getAllTransactions() {
-    this.transactionService.getAll().subscribe((transactions: any) => {
-      const order = transactions.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  private loadTransactions() {
+    this.transactionService.getAll().subscribe({
+      next: (transactionsList) => {
+        const orderedTransactions = transactionsList.sort(
+          (a: Transaction, b: Transaction) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+        );
 
-      emitEvent(EVENTS.TRANSACTIONS_UPDATED, order);
-      // emitEvent(EVENTS.TOTAL_BALANCE, calculateBalance);
+        this.transactions.set(orderedTransactions);
+        this.onTransactionsUpdated(orderedTransactions);
+      },
+      error: (error) => this.showErrorMessage('Erro ao carregar transações', error.message),
+    });
+  }
+
+  private onTransactionsUpdated(transactions: Transaction[]) {
+    const balance = this.calculateBalance(transactions);
+
+    emitEvent(EVENTS.TRANSACTIONS_UPDATED, this.transactions());
+    emitEvent('balance-updated', balance);
+  }
+
+  private calculateBalance(transactions: Transaction[]): number {
+    return transactions.reduce((acc, transaction) => {
+      if (transaction.type === TransactionType.INCOME) {
+        return acc + transaction.amount.value;
+      }
+
+      return acc - transaction.amount.value;
+    }, 0);
+  }
+
+  private showErrorMessage(summary: string, detail: string): void {
+    this.messageService.add({
+      severity: 'error',
+      summary,
+      detail,
+      life: 5000,
     });
   }
 }
-
-// private calculateBalance(transactions: TransactionDTO[]): number {
-//   return transactions.reduce((acc, transaction) => {
-//     if (transaction.type === 'RECEITA') {
-//       return acc + transaction.amount;
-//     }
-
-//     return acc - transaction.amount;
-//   }, 0);
-// }
-
-// import { CommonModule } from '@angular/common';
-// import { Component, input } from '@angular/core';
-// import { DataViewModule } from 'primeng/dataview';
-// import { TagModule } from 'primeng/tag';
-
-// @Component({
-//   selector: 'app-statement',
-//   standalone: true,
-//   imports: [CommonModule, DataViewModule, TagModule],
-//   templateUrl: './statement.component.html',
-// })
-// export class StatementComponent {
-//   transactions = input([]);
-// }
