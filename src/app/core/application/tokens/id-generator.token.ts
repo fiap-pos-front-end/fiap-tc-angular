237390776accc185a6306d90c1a540cf2df983bb
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-11-15 16:25:14 -0300

    feat(step2): remove wrong/unused folders to refactor using Clean Arch

--- src/app/core/application/tokens/id-generator.token.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { IIdGeneratorRepository } from '@fiap-tc-angular/core/domain';
-
-export const ID_GENERATOR_TOKEN = new InjectionToken<IIdGeneratorRepository>('ID_GENERATOR_TOKEN');
